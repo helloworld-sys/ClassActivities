c6d1cd6f6b051d9a6ad80a09bca7e4b9bd4a8c05
Nathan Gordon <gordon.nathan333@gamil.com>
2023-11-28 20:14:05 -0600

    Node Relationships

---
+++ Week4/4.2/4.2.2-NodeRelationships.js
@@ -0,0 +1,18 @@
+// select the root node
+const root = document.querySelector("body");
+document.getElementsByTagName("body");
+
+// select the last child of the root node
+const lastChildRoot = document.querySelector("body").lastElementChild;
+
+// select all the children of the body element
+const allChildrenBody = document.querySelector("body").children;
+
+
+// select the next sibling of the h2 node
+const nextSiblingH2 = document.querySelector("h2").nextElementSibling;
+
+
+// select the parent element of the h1 node
+const parentElementH1 = document.querySelector("h1").parentElement;
+
